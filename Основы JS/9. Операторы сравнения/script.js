62cfb7b71342fd8661dab18ba8d2c8d915fedbfe
kdvornichenko <gglade999@gmail.com>
2023-02-15 20:20:37 +0300

    task 9 complete

---
+++ Основы JS/9. Операторы сравнения/script.js
@@ -0,0 +1,7 @@
+5 > 4 // true
+'ананас' > 'яблоко' // false
+'2' > '12' // true - сравнение по первому символу, 2 > 1
+undefined == null // true - при не строгом сравнении
+undefined === null // false - при строгом сравнении
+null == '\n0\n' // false
+null === +'\n0\n' // false
